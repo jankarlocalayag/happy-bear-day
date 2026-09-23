396cca3fe3971ae47698a31ac0623ff0a5e80957
Jan Karlo <jankarlocalayag@gmail.com>
2026-09-23 18:26:41 -0400

    Update bearday.js

--- js/bearday.js
+++ js/bearday.js
@@ -4,7 +4,7 @@
 
 //first we set up a global variable that will hold our objects. 
 //variables can hold anything, including functions.
-var spacing = 25;
+var spacing = 20;
 var txtSize = 20;
 var speedinc = 1.001;
 var maxSpeed = 1;
